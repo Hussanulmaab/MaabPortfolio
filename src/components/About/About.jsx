4a0ba6c74d91edc791b7cb20a1fa674caa148074
Hussanulmaab <h.ulmaab431@gmail.com>
2024-07-17 12:39:21 +0500

    Commiting

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -24,18 +24,13 @@ const About = () => {
                             letterSpacing: "1.5px",
                             textAlign : "right",
                             }}>
-                        Gosu, Hi, My name is Hussan ul Maab and I am a Bachelor of Computer Science student from PIEAS. I possess a diverse skill set across
-                        multiple fields, including development, copywriting, and machine learning algorithms. My proficiency in the MERN
-                        stack has provided me with extensive knowledge in creating dynamic websites, while my experience in creating and
-                        deploying WordPress websites has further enhanced my expertise in this field. Additionally, I have valuable
-                        experience in creating search engine-optimized content on WordPress, which has contributed to my knowledge in
-                        content creation for online visibility.
-                        My key-expertise involve the following
-                        1 - Web development
-                        2 - Machine Learning Algorithms
-                        2 - Database making and management
-                        3 - ecommerce platform
-                        5 - UI/UX design
+                            I am a committed and driven graduate student studying computer science, and I have developed a strong portfolio of work in the areas of applied mathematics, web development, and quantum cryptography. I earned a Bachelor's degree from the Pakistan Institute of Engineering and Applied Sciences after completing my studies there with a 3.33/4 CGPA.
+
+                            In terms of real-world uses, I created a Next.js, TypeScript, Stream API, and Clerk Authentication API video conferencing application that was hosted on Vercel. My portfolio website, used React, Express, Node.js, and Particle.js to highlight my abilities and projects, is hosted on Netlify. In addition, I created a phone directory in C++, an image classification project with Convolutional Neural Networks, and an example e-commerce website utilizing the MERN stack.
+
+                            I have extensive experience with databases, web development frameworks, and computer languages. I have experience with web development technologies like Next.js, TypeScript, and the MERN stack and am skilled in Python, C/C++, Matlab, and CUDA. In addition, I have experience with several web hosting platforms such as Vercel and Netlify.
+                            
+                            I have also taken part in global initiatives like the Womanium Quantum Summer Program 2023, which allowed me to network with other businesses that are currently developing quantum technology.
                 </Typography>
             </div>
         </div>
