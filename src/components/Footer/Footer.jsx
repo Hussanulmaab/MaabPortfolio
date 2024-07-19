c479dbe9f554008a285fe520a0bc457cf1a46fb6
Hussanulmaab <h.ulmaab431@gmail.com>
2024-07-19 19:45:43 +0500

    Commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,30 +2,22 @@ import React from 'react'
 import "./footer.css"
 import {Typography} from "@mui/material"
 import { Link } from "react-router-dom"
-import { BsGithub,BsLinkedin } from "react-icons/bs"
+import { BsGithub, BsLinkedin, BsEnvelope } from "react-icons/bs"
 
 const Footer = () => {
   return (
-    <div className="footer">
-        <div>
-            <Typography variant="h5" >About Me</Typography>
-            <Typography>
-                Hi, I am Hussan ul Maab, A front-end and back-end developer, designer and freelancer.
-            </Typography>
-            <Link to="/contact" className="footerContactBtn">
-                <Typography>Contact Us</Typography>
-            </Link>
-        </div>
-        <div>
-            <Typography vairant="h5"> Social Media</Typography>
-            <a href="https://github.com/Hussanulmaab/Maabportfolio">
-                <BsGithub />
-            </a>
-            <a href="https://www.linkedin.com/in/hussan-ul-maab-92a595222/">
-                <BsLinkedin />
-            </a>
-            <Typography variant="h6" >MAIL : h.ulmaab431@gmail.com</Typography>
-        </div>
+    <div className="new-footer">
+      <div className="social-links">
+        <a href="https://www.linkedin.com/in/hussan-ul-maab-92a595222/" target="_blank" rel="noopener noreferrer">
+          <BsLinkedin /> LinkedIn
+        </a>
+        <a href="mailto:h.ulmaab431@gmail.com" target="_blank" rel="noopener noreferrer">
+          <BsEnvelope /> Email
+        </a>
+        <a href="https://github.com/Hussanulmaab/Maabportfolio" target="_blank" rel="noopener noreferrer">
+          <BsGithub /> Github
+        </a>
+      </div>
     </div>
   )
 }
