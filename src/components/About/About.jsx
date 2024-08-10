e04183de7ea322dac03ceb8326ef70560be3d070
Hussanulmaab <h.ulmaab431@gmail.com>
2024-08-10 19:45:12 +0500

    Commiting

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,42 +1,26 @@
-import React from 'react'
-import "./about.css"
-import {Typography} from "@mui/material"
-import Particle2 from '../Particle/Particle2'
-import Image from "../../images/my.jpg"
+import React from 'react';
+import './about.css';
+import { Typography } from '@mui/material';
+import Particle from '../Particle/Particle';
+import Image from '../../images/my.jpg';
 
 const About = () => {
   return (
     <div className='about'>
-        <div className="aboutContainer"></div>
-        <div className="aboutContainer2">
-
-            <div>
-                <img className='aboutAvatar' src={Image} alt="" />
-                <Typography variant='h4'>Hussan ul Maab</Typography>
-                <Typography>Full stack developer</Typography>
-            </div>
-
-            <div>
-                <Typography
-                    style={{ 
-                            wordspacing : "1.5px", 
-                            lineHeight: "25px", 
-                            letterSpacing: "1.5px",
-                            textAlign : "right",
-                            }}>
-                            I am a committed and driven graduate student studying computer science, and I have developed a strong portfolio of work in the areas of applied mathematics, web development, and quantum cryptography. I earned a Bachelor's degree from the Pakistan Institute of Engineering and Applied Sciences after completing my studies there with a 3.33/4 CGPA.
-
-                            In terms of real-world uses, I created a Next.js, TypeScript, Stream API, and Clerk Authentication API video conferencing application that was hosted on Vercel. My portfolio website, used React, Express, Node.js, and Particle.js to highlight my abilities and projects, is hosted on Netlify. In addition, I created a phone directory in C++, an image classification project with Convolutional Neural Networks, and an example e-commerce website utilizing the MERN stack.
-
-                            I have extensive experience with databases, web development frameworks, and computer languages. I have experience with web development technologies like Next.js, TypeScript, and the MERN stack and am skilled in Python, C/C++, Matlab, and CUDA. In addition, I have experience with several web hosting platforms such as Vercel and Netlify.
-                            
-                            I have also taken part in global initiatives like the Womanium Quantum Summer Program 2023, which allowed me to network with other businesses that are currently developing quantum technology.
-                </Typography>
-            </div>
+      <div className='aboutContainer'></div>
+      <div className='aboutContainer2'>
+        <div className='aboutContent'>
+          <img className='aboutAvatar' src={Image} alt='Hussan ul Maab' />
+          <Typography variant='h4' className='aboutHeading'>Hussan ul Maab</Typography>
+          <Typography className='aboutSubtitle'>Full Stack Developer</Typography>
+          <Typography className='aboutText'>
+            As a dedicated Software Developer with a solid background in Computer Science, I offer extensive experience in both Web Development and Network Software Development. Proficient in languages like Python, C++, and TypeScript, I have successfully utilized frameworks such as Next.js and the MERN Stack to build robust web applications. My professional experience includes developing a secure communication system and error correction software for networks at the National Institute of Lasers and Optronics (NILOP), demonstrating my ability to handle complex projects. I have also created a video conferencing application and an e-commerce site, showcasing my web development skills. With a Bachelor's degree from the Pakistan Institute of Engineering and Applied Sciences (CGPA: 3.38/4) and hands-on experience in various projects, I am well-equipped to contribute effectively to any software development team.
+          </Typography>
         </div>
-        <Particle2 />
-    </div>  
-  )
-}
+      </div>
+      <Particle />
+    </div>
+  );
+};
 
-export default About
+export default About;
